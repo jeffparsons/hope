c9f11fe7aa9345a70e892d9b0070ba783a94b8bf
Jeff Parsons <jeff@parsons.io>
2024-07-08 00:23:29 +1000

    Add another test and tidy up test helpers
    This fails. So next will be fixing up the impl so that it works. :P

--- wrapper/tests/integration_tests.rs
+++ wrapper/tests/integration_tests.rs
@@ -1,11 +1,68 @@
-use std::{
-    path::Path,
-    process::{Command, Stdio},
-};
+use std::process::{Command, Stdio};
 
-use tempfile::tempdir;
+use tempfile::{tempdir, TempDir};
 
 const WRAPPER_PATH: &str = env!("CARGO_BIN_EXE_wrapper");
+
+// Simple case where the wrapper shouldn't do anything.
+#[test]
+fn build_crate_with_no_deps() {
+    let package = Package::new();
+    package.build();
+}
+
+// One dependency, without any build script.
+#[test]
+fn build_crate_with_simple_dep() {
+    let package = Package::new();
+    package.add("anyhow@1.0.0");
+    package.build();
+}
+
+//
+// Test helpers
+//
+
+struct Package {
+    dir: TempDir,
+}
+
+impl Package {
+    fn new() -> Self {
+        let package = Self {
+            dir: tempdir().unwrap(),
+        };
+        package.init();
+        package
+    }
+
+    fn init(&self) {
+        assert!(cargo()
+            .args(["init", "--name", "foo"])
+            .current_dir(self.dir.path())
+            .status()
+            .unwrap()
+            .success());
+    }
+
+    fn add(&self, dep: &str) {
+        assert!(cargo()
+            .args(["add", dep])
+            .current_dir(self.dir.path())
+            .status()
+            .unwrap()
+            .success());
+    }
+
+    fn build(&self) {
+        assert!(cargo()
+            .arg("build")
+            .current_dir(self.dir.path())
+            .status()
+            .unwrap()
+            .success());
+    }
+}
 
 // Default to using the wrapper to exercise it as much as possible;
 // if we want to do things without it, we can have a version of this
@@ -18,20 +75,4 @@ fn cargo() -> Command {
     command.stdout(Stdio::null());
     command.stderr(Stdio::null());
     command
-}
-
-fn cargo_init(path: &Path) {
-    assert!(cargo()
-        .args(["init", "--name", "foo"])
-        .current_dir(path)
-        .status()
-        .unwrap()
-        .success());
-}
-
-// Simple case where the wrapper shouldn't do anything.
-#[test]
-fn build_crate_with_no_deps() {
-    let dir = tempdir().unwrap();
-    cargo_init(dir.path());
 }
